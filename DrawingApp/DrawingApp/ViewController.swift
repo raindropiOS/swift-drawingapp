6371f7da4b77c6af00a9ed6ce9f7a72eadd78b66
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-28 11:53:30 +0900

    addRandomSquares 메소드 추가
    개수, width, height를 파라미터 받아 뷰의 랜덤한 위치, 색상, 투명도로 생성

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addRandomSquares(count: 4, width: 150, height: 120)
     }
-
-
+    
+    func addRandomSquares(count: Int, width: Double, height: Double) {
+        guard count > 0 else { return }
+        
+        // 좌표를 구할 때 어느 것을 써야하는지 고민.UIScreen.main.bounds.size.width or self.view.frame.size.width
+        for num in 1...count {
+            let name = "Rect \(num)"
+            let xBoundary = width...self.view.frame.size.width
+            let yBoundary = 0...(self.view.frame.size.height - height)
+            let x = Double.random(in: xBoundary)
+            let y = Double.random(in: yBoundary)
+            
+            let square = SquareFactory.makeSquare(x: x, y: y, width: width, height: height, kind: .random)
+            self.view.addSubview(square as! UIView)
+        }
+    }
 }
 
