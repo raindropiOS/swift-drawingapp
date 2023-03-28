e527db872663747cc320d4d3ada35ddd042c98bd
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-28 17:30:36 +0900

    addRandomSquares 메소드에 로그 추가
    RandomSquare의 description 프라퍼티를 통한 log 출력

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import OSLog
 
 class ViewController: UIViewController {
     
@@ -16,17 +17,22 @@ class ViewController: UIViewController {
     
     func addRandomSquares(count: Int, width: Double, height: Double) {
         guard count > 0 else { return }
+        let customLogger = Logger(subsystem: "com.eddie.DrawingApp", category: "ViewController")
         
-        // 좌표를 구할 때 어느 것을 써야하는지 고민.UIScreen.main.bounds.size.width or self.view.frame.size.width
+        // 좌표를 구할 때 어느 것을 써야하는지 다음 스텝에서 고민 예정 .UIScreen.main.bounds.size.width or self.view.frame.size.width
         for num in 1...count {
-            let name = "Rect \(num)"
+            let name = "Rect\(num)"
             let xBoundary = width...self.view.frame.size.width
             let yBoundary = 0...(self.view.frame.size.height - height)
             let x = Double.random(in: xBoundary)
             let y = Double.random(in: yBoundary)
-            
-            let square = SquareFactory.makeSquare(x: x, y: y, width: width, height: height, kind: .random)
-            self.view.addSubview(square as! UIView)
+            if let randomSquare = SquareFactory.makeSquare(x: x, y: y, width: width, height: height, kind: .random) as? RandomSquare {
+                // TODO: 뷰에 추가하기
+                customLogger.log("\(name) \(randomSquare.description)")
+            }
+            else {
+                customLogger.log("SquareFactory failed to generate RandomSquare in addRandomSquares")
+            }
         }
     }
 }
