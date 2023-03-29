63613c392c9e9ab20b7cf0c7308722b044e903d6
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-29 16:47:38 +0900

    Plane 추가
    * components: [Shape] 프라퍼티
    * count 연산 프라퍼티. components.count 리턴
    * addShape 메소드
    * returnComponent(at index: Int)

---
+++ DrawingApp/DrawingApp/Plane.swift
@@ -0,0 +1,23 @@
+//
+//  Plane.swift
+//  DrawingApp
+//
+//  Created by 에디 on 2023/03/29.
+//
+
+import Foundation
+
+struct Plane {
+    private var components: [Shape]
+    var count: Int {
+        components.count
+    }
+    
+    mutating func addShape(_ shape: Shape) {
+        components.append(shape)
+    }
+    
+    func returnComponent(at index: Int) -> Shape? {
+        components[index]
+    }
+}
