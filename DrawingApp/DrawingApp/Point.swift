d445415180a95ada079dc33c4f60230517c83d68
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-29 14:19:10 +0900

    Size, Point 추가

---
+++ DrawingApp/DrawingApp/Point.swift
@@ -0,0 +1,13 @@
+//
+//  Point.swift
+//  DrawingApp
+//
+//  Created by 에디 on 2023/03/29.
+//
+
+import Foundation
+
+struct Point {
+    var x: Double
+    var y: Double
+}
---
+++ DrawingApp/DrawingApp/Size.swift
@@ -0,0 +1,13 @@
+//
+//  Size.swift
+//  DrawingApp
+//
+//  Created by 에디 on 2023/03/29.
+//
+
+import Foundation
+
+struct Size {
+    var width: Double
+    var height: Double
+}
