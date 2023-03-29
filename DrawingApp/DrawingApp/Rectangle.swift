361c9b5c6a7ef7ca3bf1e1af3ed5832ea22ef2fb
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-29 15:11:05 +0900

    Rectangle(구 RandomSquare)의 프라퍼티명 bgColor -> backgroundColor 로 변경했습니다.

--- DrawingApp/DrawingApp/Rectangle.swift
+++ DrawingApp/DrawingApp/Rectangle.swift
@@ -11,20 +11,20 @@ class Rectangle: Shape {
     let id: Id
     let point: Point
     let size: Size
-    let bgColor: (CGFloat, CGFloat, CGFloat)
+    let backgroundColor: (CGFloat, CGFloat, CGFloat)
     let alpha: CGFloat
     
     init(id: Id, point: Point, size: Size, bgColor: (CGFloat, CGFloat, CGFloat), alpha: CGFloat) {
         self.id = id
         self.point = point
         self.size = size
-        self.bgColor = bgColor
+        self.backgroundColor = bgColor
         self.alpha = alpha
     }
 }
 
 extension Rectangle: CustomStringConvertible {
     var description: String {
-        "(\(id.value)), X:\(point.x), Y:\(point.y), W\(size.width), H\(size.height), R:\(bgColor.0), G:\(bgColor.1), B:\(bgColor.2)"
+        "(\(id.value)), X:\(point.x), Y:\(point.y), W\(size.width), H\(size.height), R:\(backgroundColor.0), G:\(backgroundColor.1), B:\(backgroundColor.2)"
     }
 }
