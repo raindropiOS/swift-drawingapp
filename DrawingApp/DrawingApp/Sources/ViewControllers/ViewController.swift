d7a125ef4d3f512c95cf08ae8293745dd7fc5658
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-31 11:47:43 +0900

    랜덤한 사각형 그리는 메소드 추가
    retangleButtonPressed
    generateRandomPoint
    draw(rectangle: Rectangle)
    drawRandomRectangle
    returnCGRectFrom
    returnUIColorFrom

--- DrawingApp/DrawingApp/Sources/ViewControllers/ViewController.swift
+++ DrawingApp/DrawingApp/Sources/ViewControllers/ViewController.swift
@@ -15,20 +15,28 @@ class ViewController: UIViewController {
         let point = tapGestureRecognizer.location(in: self.view)
     }
     
+    @IBAction func rectangleButtonPressed(_ sender: Any) {
+        drawRandomRectangle()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         addRandomRectangles(count: 4, size: Size(width: 150, height: 120))
+        let width = self.view.frame.width
+        let height = self.view.frame.height
+        let boundWidth = self.view.bounds.width
+        let boundHeight = self.view.bounds.height
+        print("")
     }
     
     func addRandomRectangles(count: Int, size: Size) {
         guard count > 0 else { return }
         let customLogger = Logger(subsystem: "com.eddie.DrawingApp", category: "ViewController")
         
-        // 좌표를 구할 때 어느 것을 써야하는지 다음 스텝에서 고민 예정 .UIScreen.main.bounds.size.width or self.view.frame.size.width
         for num in 1...count {
             let name = "Rect\(num)"
-            let xBoundary = size.width...self.view.frame.size.width
-            let yBoundary = 0...(self.view.frame.size.height - size.height)
+            let xBoundary = 0...self.view.frame.size.width - size.width
+//            let yBoundary = 0...self.view.frame.size.height - size.height
+            let yBoundary = size.height...self.view.frame.size.height
             let x = Double.random(in: xBoundary)
             let y = Double.random(in: yBoundary)
             let point = Point(x: x, y: y)
@@ -40,6 +48,46 @@ class ViewController: UIViewController {
                 customLogger.log("SquareFactory failed to generate RandomSquare in addRandomSquares")
             }
         }
+    }
+    
+    func generateRandomPoint(basedOn size: Size) -> Point {
+//        let xBoundary = 0...self.view.frame.size.width - size.width
+        let xBoundary = 0...self.view.frame.width - size.width
+//            let yBoundary = 0...self.view.frame.size.height - size.height
+//        let yBoundary = size.height...self.view.frame.size.height
+        let yBoundary = size.height...self.view.frame.height
+        let x = Double.random(in: xBoundary)
+        let y = Double.random(in: yBoundary)
+        
+        return Point(x: x, y: y)
+    }
+    
+    func draw(rectangle: Rectangle) {
+        let cgRect = returnCGRectFrom(point: rectangle.point, size: rectangle.size)
+        let uiColor = returnUIColorFrom(color: rectangle.backgroundColor, alpha: rectangle.alpha)
+        let uiView = UIView(frame: cgRect)
+        
+        uiView.backgroundColor = uiColor
+        self.view.addSubview(uiView)
+    }
+    
+    func drawRandomRectangle() {
+        let size = Size(width: 150, height: 120)
+        let randomPoint = generateRandomPoint(basedOn: size)
+        
+        if let randomRectangle = ShapeFactory.makeShape(point: randomPoint, size: size, kind: .random) as? Rectangle {
+            draw(rectangle: randomRectangle)
+        } else {
+            
+        }
+    }
+    
+    func returnCGRectFrom(point: Point, size: Size) -> CGRect {
+        CGRect(x: Int(point.x), y: Int(point.y), width: Int(size.width), height: Int(size.height))
+    }
+    
+    func returnUIColorFrom(color: Color, alpha: Int) -> UIColor {
+        UIColor(red: CGFloat(color.r), green: CGFloat(color.g), blue: CGFloat(color.b), alpha: CGFloat(alpha))
     }
 }
 
