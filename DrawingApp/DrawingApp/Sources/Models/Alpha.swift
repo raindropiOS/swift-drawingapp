9f1d1c0788ee5669c1421838a34a1b3539b2b541
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-04-03 23:02:11 +0900

    Alpha 타입 추가

---
+++ DrawingApp/DrawingApp/Sources/Models/Alpha.swift
@@ -0,0 +1,22 @@
+//
+//  Alpha.swift
+//  DrawingApp
+//
+//  Created by 에디 on 2023/04/03.
+//
+
+import Foundation
+
+enum Alpha: Double {
+    case one = 0.1
+    case two = 0.2
+    case three = 0.3
+    case four = 0.4
+    case five = 0.5
+    case six = 0.6
+    case seven = 0.7
+    case eight = 0.8
+    case nine = 0.9
+    case ten = 1.0
+}
+
