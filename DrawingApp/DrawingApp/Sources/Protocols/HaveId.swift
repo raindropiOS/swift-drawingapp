99cccf835dc481b1f0343620129b66d584e53675
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-04-03 17:27:47 +0900

    RectangleView HaveId 추가

---
+++ DrawingApp/DrawingApp/Sources/Protocols/HaveId.swift
@@ -0,0 +1,12 @@
+//
+//  haveId.swift
+//  DrawingApp
+//
+//  Created by 에디 on 2023/04/03.
+//
+
+import Foundation
+
+protocol HaveId {
+    var id: Id { get }
+}
---
+++ DrawingApp/DrawingApp/Sources/Views/RectangleView.swift
@@ -0,0 +1,23 @@
+//
+//  RectangleView.swift
+//  DrawingApp
+//
+//  Created by 에디 on 2023/04/03.
+//
+
+import UIKit
+
+class RectangleView: UIView, HaveId {
+    var id: Id
+    
+    init(frame: CGRect, id: Id) {
+        self.id = id
+        super.init(frame: frame)
+    }
+    
+    // 어쩔 수 없이 작성한 init
+    required init?(coder: NSCoder) {
+        id = Id(value: "error")
+        super.init(coder: coder)
+    }
+}
