6a0632b45b76c7f7c522fa290480123b82b222ed
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-04-05 10:43:01 +0900

    Colorable 프로토콜 추가
    backgroundColor, alpha로 구성

---
+++ DrawingApp/DrawingApp/Sources/Protocols/Colorable.swift
@@ -0,0 +1,13 @@
+//
+//  Colorable.swift
+//  DrawingApp
+//
+//  Created by 에디 on 2023/04/05.
+//
+
+import Foundation
+
+protocol Colorable {
+    var backgroundColor: Color { get }
+    var alpha: Alpha { get }
+}
