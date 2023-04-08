159a432dad9ad2e3adfc4747f22a18a3a99c1811
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-04-09 00:35:14 +0900

    프라퍼티 래퍼 RangedValue 추가

--- DrawingApp/DrawingApp/Sources/Models/RangedValue.swift
+++ DrawingApp/DrawingApp/Sources/Models/RangedValue.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct RangedValue<V: Comparable> {
+    private var value: V
+    private var maximum: V
+    private var minimum: V
+    
+    var wrappedValue: V {
+        get { return  value }
+        set {
+            if newValue > maximum { value = maximum; return }
+            if newValue < minimum { value = minimum; return }
+            value = newValue
+        }
+    }
+    
+    init(wrappedValue: V, minimum: V, maximum: V) {
+        value = wrappedValue
+        self.minimum = minimum
+        self.maximum = maximum
+        assert(value >= minimum && value <= maximum)
+    }
+}
